07b3ae44e1158b8d1d053899cfb9fbcd02a8e828
Rosux <jcsbt2015@gmail.com>
2024-02-14 22:58:28 +0100

    added logo and WriteCenter method

--- src/Program.cs
+++ src/Program.cs
@@ -4,11 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadKey();
+        // some intro shit
         Console.Clear();
-        // Console.BackgroundColor = ConsoleColor.Blue;
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("UwU ONICHAN");
+        string spacing = "";
+        for (int i = 0; i < (Console.WindowWidth-23) / 2; i++)
+        {
+            spacing += " ";
+        }
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        WriteCenter($"\n\n   ▄████████  ▄██████▄ \n  ███    ███ ███    ███\n  ███    █▀  ███    ███\n ▄███▄▄▄     ███    ███\n▀▀███▀▀▀     ███    ███\n  ███        ███    ███\n  ███        ███    ███\n  ███         ▀██████▀   ");
+        Console.ForegroundColor = ConsoleColor.White;
+        WriteCenter("\n(GOTY Edition)\n\n");
+        
+    }
+
+    static void WriteCenter(string text)
+    {
+        // split text at lines
+        string[] lines = text.Split('\n');
+
+        // find longest line
+        int maxLength = lines.OrderByDescending(l=>l.Length).First().Length;
+
+        // Calculate left padding for each line
+        int screenWidth = Console.WindowWidth;
+        foreach (string line in lines)
+        {
+            int padding = Math.Max((screenWidth - maxLength) / 2, 0);
+            Console.Write(new string(' ', padding));
+            Console.WriteLine(line);
+        }
     }
 }
+
+/*
+      .,-,.\n
+   _-'/   \'-_\n
+_-'  /     \  '-_\n
+|'--|-------|--'|\n
+| / \       / \ |\n
+|/   \ 21  /   \|\n
+/     \   /     \\n
+|------\ /------|\n
+ '-._   |   _.-'\n
+     '-.'.-'\n
+*/
+
+//         ▄████████  ▄██████▄ \n
+//        ███    ███ ███    ███\n
+//        ███    █▀  ███    ███\n
+//       ▄███▄▄▄     ███    ███\n
+//      ▀▀███▀▀▀     ███    ███\n
+//        ███        ███    ███\n
+//        ███        ███    ███\n
+//        ███         ▀██████▀ \n
