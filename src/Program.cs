4fa6391cee89130e8b085b93994cde24a1cd79dc
Rosux <jcsbt2015@gmail.com>
2024-02-17 23:20:52 +0100

    more dialogue changes and fixed the visuals

--- src/Program.cs
+++ src/Program.cs
@@ -4,22 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        WriteCenter($"\n\n   ▄████████  ▄██████▄ \n  ███    ███ ███    ███\n  ███    █▀  ███    ███\n ▄███▄▄▄     ███    ███\n▀▀███▀▀▀     ███    ███\n  ███        ███    ███\n  ███        ███    ███\n  ███         ▀██████▀   ");
-        Console.ForegroundColor = ConsoleColor.White;
-        WriteCenter("\n(GOTY Edition)\n\n");
-
+        Console.Title = "FO (GOTY Edition)";
+        Console.CursorVisible = false;
+        WriteLogo();
 
         Dialogue peter = new Dialogue();
         peter.AddNode("1", "Hey! I'm peter griffin.", new List<Option>(){
             new Option("Hi peter...", "1.1"),
             new Option("Not true!", "1.2"),
         });
-        peter.AddNode("1.2", "It's true, I am really peter griffin.", new List<Option>());
         peter.AddNode("1.1", "Bye.", new List<Option>());
-
+        peter.AddNode("1.2", "It's true, I am really peter griffin.", new List<Option>());
 
         Talk(peter);
     }
@@ -29,6 +24,8 @@ class Program
         int currentChoice = 0;
         while (true)
         {
+            Console.Clear();
+            WriteLogo();
             Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine($"{dialogueTree.CurrentNode.Text}");
             if (dialogueTree.GetChoices().Count == 0)
@@ -38,7 +35,7 @@ class Program
             // print options
             foreach (KeyValuePair<int, string> text in dialogueTree.GetChoices())
             {
-                if (text.Key == currentChoice) { Console.BackgroundColor = ConsoleColor.White; }
+                if (text.Key == currentChoice) { Console.BackgroundColor = ConsoleColor.DarkGray; }
                 Console.WriteLine($">{text.Key+1}: {text.Value}");
                 Console.BackgroundColor = ConsoleColor.Black;
             }
@@ -49,16 +46,20 @@ class Program
                 key = Console.ReadKey(true).Key;
                 if (key == ConsoleKey.UpArrow)
                 {
-                    currentChoice -= 1;
+                    currentChoice--;
                 }
-                currentChoice = Clamp(currentChoice, 0, dialogueTree.GetChoices().Count-1);
+                else if (key == ConsoleKey.DownArrow)
+                {
+                    currentChoice++;
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    dialogueTree.Step(currentChoice);
+                    currentChoice = 0;
+                    break;
+                }
+                currentChoice = Math.Clamp(currentChoice, 0, dialogueTree.GetChoices().Count-1);
             } while (key != ConsoleKey.UpArrow && key != ConsoleKey.DownArrow && key != ConsoleKey.Enter);
-
-            // int option = Convert.ToInt32(Console.ReadLine())-1;
-            // if (option >= 0 && option < dialogueTree.GetChoices().Count)
-            // {
-            //     dialogueTree.Step(option);
-            // }
         }
         WriteCenter("Press any key to continue...");
         Console.ReadKey(true);
@@ -80,6 +81,16 @@ class Program
             Console.Write(new string(' ', padding));
             Console.WriteLine(line);
         }
+    }
+
+    static void WriteLogo()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        WriteCenter($"\n\n   ▄████████  ▄██████▄ \n  ███    ███ ███    ███\n  ███    █▀  ███    ███\n ▄███▄▄▄     ███    ███\n▀▀███▀▀▀     ███    ███\n  ███        ███    ███\n  ███        ███    ███\n  ███         ▀██████▀   ");
+        Console.ForegroundColor = ConsoleColor.White;
+        WriteCenter("\n(GOTY Edition)\n\n");
     }
 }
 
